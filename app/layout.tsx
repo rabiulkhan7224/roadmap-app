fced376e849b6c979fb5fdce9fd4bf691aa34e97
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-06-21 14:17:24 +0600

    added register page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import navbar from './../components/navbar';
 import Navbar from "./../components/navbar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,8 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Toaster position="top-right"
+  reverseOrder={false}/>
       </body>
     </html>
   );
---
+++ app/register/page.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+
+export default function RegisterPage() {
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const onSubmit = async (data: any) => {
+    try {
+      const responce  = await axios.post(`${process.env.NEXT_PUBLIC_API}/api/auth/register`, data);
+      if (responce.status === 201) {
+        setError('');
+        toast.success('Registration successful! Please log in.');
+        // Redirect to login page after successful registration
+        router.push('/login');
+      }
+
+      
+    } catch (err) {
+      setError('Something went wrong');
+        console.error('Registration error:', err);
+            toast.error('Registration failed. Please try again.');
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-300">
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-8 shadow-lg rounded-lg w-full max-w-md space-y-4">
+        <h2 className="text-2xl font-bold text-center text-blue-700">Register</h2>
+
+        {error && <p className="text-red-500 text-sm">{error}</p>}
+
+        <input
+          {...register('name')}
+          type="text"
+          placeholder="Full Name"
+          className="w-full border px-3 py-2 rounded-md"
+          required
+        />
+        <input
+          {...register('email')}
+          type="email"
+          placeholder="Email"
+          className="w-full border px-3 py-2 rounded-md"
+          required
+        />
+        <input
+          {...register('password')}
+          type="password"
+          placeholder="Password"
+          className="w-full border px-3 py-2 rounded-md"
+          required
+        />
+
+        <button type="submit" className="w-full bg-green-600 text-white py-2 rounded-md hover:bg-green-700">
+          Register
+        </button>
+
+        <p className="text-sm text-center">
+          Already have an account? <a href="/login" className="text-blue-600 hover:underline">Login</a>
+        </p>
+      </form>
+    </div>
+  );
+}
